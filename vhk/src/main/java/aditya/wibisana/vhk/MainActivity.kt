cdbd7011c65b49c5c500ad55f001011bc8ab17e5
Aditya Wibisana <aditya.wibisana@gmail.com>
2025-05-04 05:09:14 +0700

    Add sample code

--- vhk/src/main/java/aditya/wibisana/vhk/MainActivity.kt
+++ vhk/src/main/java/aditya/wibisana/vhk/MainActivity.kt
@@ -1,10 +1,12 @@
 package aditya.wibisana.vhk
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.menny.android.anysoftkeyboard.LauncherSettingsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        startActivity(Intent(this, LauncherSettingsActivity::class.java))
     }
 }
